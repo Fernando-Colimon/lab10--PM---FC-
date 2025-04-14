23d1be0c309f290b9aef27d7f93e2fa0e82ff17f
PrestonMichaels <144281040+PrestonMichaels@users.noreply.github.com>
2025-04-14 00:14:05 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -13,5 +13,42 @@ def add(a, b):
 
 def sub(a,b):
     return a-b
+    def test_multiply(self): # 3 assertions
+        pass
 
+    def test_divide(self): # 3 assertions
+        pass
+    ##########################
+
+    # Partner 2
+    def test_divide_by_zero(self): # 1 assertion
+        # call division function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     div(0, 5)
+        pass
+
+    def test_logarithm(self): # 3 assertions
+        pass
+
+    def test_log_invalid_base(self): # 1 assertion
+        # use same technique from test_divide_by_zero
+        pass
+    ##########################
+    
+    # Partner 1
+    def test_log_invalid_argument(self): # 1 assertion
+        # call log function inside, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #     logarithm(0, 5)
+        pass
+
+    def test_hypotenuse(self): # 3 assertions
+        pass
+
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        # with self.assertRaises(<INSERT_ERROR_TYPE>):
+        #    square_root(NUM)
+        # Test basic function
+        pass
 
