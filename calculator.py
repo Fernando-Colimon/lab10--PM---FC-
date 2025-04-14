3934b08f021948e4e35033aceaeb6a8b19b7bd81
PrestonMichaels <144281040+PrestonMichaels@users.noreply.github.com>
2025-04-14 00:16:50 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,44 +11,32 @@ One function per operation, in order.
 def add(a, b): 
     return a + b
 
-def sub(a,b):
+def subtract(a,b):
     return a-b
-    def test_multiply(self): # 3 assertions
-        pass
+def mul(self): # 3 assertions
+    pass
 
-    def test_divide(self): # 3 assertions
-        pass
+def div(self): # 3 assertions
+    pass
     ##########################
 
     # Partner 2
-    def test_divide_by_zero(self): # 1 assertion
-        # call division function inside, example:
-        # with self.assertRaises(<INSERT_ERROR_TYPE>):
-        #     div(0, 5)
-        pass
 
-    def test_logarithm(self): # 3 assertions
-        pass
 
-    def test_log_invalid_base(self): # 1 assertion
-        # use same technique from test_divide_by_zero
-        pass
-    ##########################
-    
-    # Partner 1
-    def test_log_invalid_argument(self): # 1 assertion
-        # call log function inside, example:
-        # with self.assertRaises(<INSERT_ERROR_TYPE>):
-        #     logarithm(0, 5)
-        pass
+def logarithm(self): # 3 assertions
+    pass
 
-    def test_hypotenuse(self): # 3 assertions
-        pass
 
-    def test_sqrt(self): # 3 assertions
+def exp(self):
+    pass
+
+def hypotenuse(self): # 3 assertions
+    pass
+
+def square_root(self): # 3 assertions
         # Test for invalid argument, example:
         # with self.assertRaises(<INSERT_ERROR_TYPE>):
         #    square_root(NUM)
         # Test basic function
-        pass
+    pass
 
